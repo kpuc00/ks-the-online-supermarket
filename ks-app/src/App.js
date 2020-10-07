e601cd720650fded9448a6c8c3a7f6093b62e65e
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-10-07 13:41:00 +0200

    installed bootstrap

--- ks-app/src/App.js
+++ ks-app/src/App.js
@@ -1,31 +1,13 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import CustomersList from "./CustomersList";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-const ReactDOM = require("react-dom");
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {customers: []};
-    }
-
-    componentDidMount() {
-        fetch("http://localhost:8080/customers").then((response) => {
-            response.json().then((customers) => this.setState({customers: customers}));
-        });
-    }
-render() {
-    return(
-
-        <div className="App">
-            <header className="App-header">
-                <CustomersList customers={this.state.customers} />
-            </header>
-        </div>
-    )
-};
+export default function App() {
+  return (
+      <p>KS</p>
+  );
 }
-
-export default App;
