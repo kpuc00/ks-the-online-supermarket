b804be483d263fc78ba14939031c4e7b8076f6d4
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-10-07 23:22:11 +0200

    Removed unwanted files marked in .gitignore

--- ks-app/src/App.js
+++ ks-app/src/App.js
@@ -5,9 +5,44 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function App() {
   return (
-      <p>KS</p>
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">KS Supermarket</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+            <Nav.Link href="#features">Products</Nav.Link>
+            <Nav.Link href="#pricing">Offers</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+        </Nav>
+        <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+            </Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+        <Container>
+            <Row>
+                <Col>1 of 1</Col>
+            </Row>
+        </Container>
+      </div>
   );
 }
