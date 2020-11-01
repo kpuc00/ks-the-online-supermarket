b3bbcd3d9908cfc6ff1f6afd16fe883dd949db45
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-11-01 19:54:50 +0100

    App: Renamed folder resources to controllers

--- ks-api/src/main/java/com/ks/service/ks/controllers/HomeController.java
+++ ks-api/src/main/java/com/ks/service/ks/controllers/HomeController.java
@@ -1,7 +1,6 @@
-package com.ks.service.ks.resources;
+package com.ks.service.ks.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
