2111512364c75506da2d9997e8cc36a7029ac933
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-11-01 23:39:19 +0100

    API: Initial authorization

--- ks-api/src/main/java/com/ks/service/ks/KsApplication.java
+++ ks-api/src/main/java/com/ks/service/ks/KsApplication.java
@@ -2,8 +2,10 @@ package com.ks.service.ks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.ks.service.ks.repository")
 public class KsApplication {
 
     public static void main(String[] args) {
