4f7d6ab43db39f03501bb2dd7d1313d036115b17
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-11-03 23:31:27 +0100

    Api: Deleted CustomerRepository

--- ks-api/src/main/java/com/ks/service/ks/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ks.service.ks.repository;
-
-import com.ks.service.ks.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called customerRepository
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {}
