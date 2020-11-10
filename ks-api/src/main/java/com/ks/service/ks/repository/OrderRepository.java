8a8d9c96e3b3805c23ae3a4725c6eb073e3bafe8
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-11-10 15:12:01 +0100

    Api: Removed old order classes

--- ks-api/src/main/java/com/ks/service/ks/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ks.service.ks.repository;
-
-import com.ks.service.ks.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {}
