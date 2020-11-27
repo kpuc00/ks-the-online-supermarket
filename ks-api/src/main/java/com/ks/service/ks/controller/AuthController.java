d9f3bb4b4e09b0a66144353e3d302538408a3b36
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-11-27 17:55:24 +0100

    API: Auth controller fixes

--- ks-api/src/main/java/com/ks/service/ks/controller/AuthController.java
+++ ks-api/src/main/java/com/ks/service/ks/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -46,7 +47,7 @@ public class AuthController {
     JwtUtils jwtUtils;
 
     @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername().toLowerCase(), loginRequest.getPassword()));
@@ -56,7 +57,7 @@ public class AuthController {
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
+                .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(new JwtResponse(jwt,
@@ -72,14 +73,14 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-        if (userService.existsByUsername(signUpRequest.getUsername().toLowerCase())) {
+    public ResponseEntity registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (Boolean.TRUE.equals(userService.existsByUsername(signUpRequest.getUsername().toLowerCase()))) {
             return ResponseEntity
                     .badRequest()
                     .body(new MessageResponse("Username is already taken!"));
         }
 
-        if (userService.existsByEmail(signUpRequest.getEmail())) {
+        if (Boolean.TRUE.equals(userService.existsByEmail(signUpRequest.getEmail()))) {
             return ResponseEntity
                     .badRequest()
                     .body(new MessageResponse("Email is already in use!"));
@@ -97,29 +98,29 @@ public class AuthController {
 
         Set<String> strRoles = signUpRequest.getRole();
         Set<Role> roles = new HashSet<>();
-
+        String missingRole = "Role is not found.";
         if (strRoles == null) {
             Role userRole = roleService.findByName(ERole.ROLE_USER)
-                    .orElseThrow(() -> new RuntimeException("Role is not found."));
+                    .orElseThrow(() -> new RuntimeException(missingRole));
             roles.add(userRole);
         } else {
             strRoles.forEach(role -> {
                 switch (role) {
                     case "admin":
                         Role adminRole = roleService.findByName(ERole.ROLE_ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Role is not found."));
+                                .orElseThrow(() -> new RuntimeException(missingRole));
                         roles.add(adminRole);
 
                         break;
                     case "mod":
                         Role modRole = roleService.findByName(ERole.ROLE_MODERATOR)
-                                .orElseThrow(() -> new RuntimeException("Role is not found."));
+                                .orElseThrow(() -> new RuntimeException(missingRole));
                         roles.add(modRole);
 
                         break;
                     default:
                         Role userRole = roleService.findByName(ERole.ROLE_USER)
-                                .orElseThrow(() -> new RuntimeException("Role is not found."));
+                                .orElseThrow(() -> new RuntimeException(missingRole));
                         roles.add(userRole);
                 }
             });
